010a2447939aa86bd769901a72ff3579d58558de
hardiksingh98 <hardik.singh@turtlemint.com>
2020-10-04 09:20:31 +0530

    Claims, verify token

--- src/main/java/com/usermgmt/dem/constants/SecurityConstant.java
+++ src/main/java/com/usermgmt/dem/constants/SecurityConstant.java
@@ -6,7 +6,6 @@ public class SecurityConstant {
     public static final String JWT_TOKEN_HEADER = "Jwt-Token";
     public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
     public static final String GET_ARRAYS_LLC = "Get Arraus LLC";
-    public static final String GET_ARRAYS_AUTHORITIES = "User Management Portal";
     public static final String GET_ARRAYS_ADMINISTRATION = "User Management Portal";
     public static final String AUTHORITIES = "Authorities";
     public static final String FORBIDDEN_MESSAGE = "You need to login to access this page";
--- src/main/java/com/usermgmt/dem/resource/utility/JWTTokenProvider.java
+++ src/main/java/com/usermgmt/dem/resource/utility/JWTTokenProvider.java
@@ -1,12 +1,23 @@
 package com.usermgmt.dem.resource.utility;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import static  com.usermgmt.dem.constants.SecurityConstant.*;
+import static java.util.Arrays.stream;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.usermgmt.dem.domain.UserPrincipal;
 import lombok.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class JWTTokenProvider {
 
@@ -27,7 +38,54 @@ public class JWTTokenProvider {
 
     }
 
+    public List<GrantedAuthority> getAuthorities(String token)
+    {
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+
+
+
+    }
+
+    private String[] getClaimsFromToken(String token) {
+
+        JWTVerifier jwtVerifier =  getVerifier();
+        return jwtVerifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+
+
+    }
+
+    private JWTVerifier getVerifier() {
+
+        JWTVerifier jwtVerifier;
+        try{
+            Algorithm algorithm = Algorithm.HMAC512(secret);
+            jwtVerifier = JWT.require(algorithm).withIssuer(GET_ARRAYS_LLC).build();
+
+
+        }
+        catch(JWTVerificationException exception){
+           throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+         }
+        return jwtVerifier;
+
+    }
+
+
     private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
+        List<String> authorities = new ArrayList<>();
+        for(GrantedAuthority grantedauthority: userPrincipal.getAuthorities())
+        {
+            authorities.add(grantedauthority.getAuthority());
+        }
+
+
+        //return authorites in form of string
+        return authorities.toArray(new String[0]);
+
+
+
+
     }
 
 
