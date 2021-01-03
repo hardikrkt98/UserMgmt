b824336489af7824c9499405503ea99d009abed3
hardiksingh98 <hardik.singh@turtlemint.com>
2021-01-03 18:06:29 +0530

    consume API key from env bashrc

--- src/main/java/com/usermgmt/dem/Service/NotificationsService.java
+++ src/main/java/com/usermgmt/dem/Service/NotificationsService.java
@@ -3,6 +3,8 @@ package com.usermgmt.dem.Service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.usermgmt.dem.domain.HttpResponse;
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -10,20 +12,18 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import javax.mail.*;
-import javax.mail.internet.*;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Properties;
+import org.springframework.core.env.Environment;
 
 @Service
 public class NotificationsService {
 
-
+    @Autowired
+    private Environment env;
 
  public void sendEmail(String email,String passWord) throws MessagingException {
 
      String url  = "https://api.mailgun.net/v3/codeitnow.me/messages";
-
+     String api_key  = env.getProperty("apikey");
      HttpHeaders httpHeaders = new HttpHeaders();
      httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
      MultiValueMap<String, String> multiValueMap= new LinkedMultiValueMap<String, String>();
@@ -31,7 +31,7 @@ public class NotificationsService {
      multiValueMap.add("from","support@codeitnow.me");
      multiValueMap.add("subject","Password to login");
      multiValueMap.add("text","You can login to codeitnow.me through password "+passWord);
-     httpHeaders.setBasicAuth("api","bcfaab63edd03f250290b35bf6531707-c50a0e68-f1c36051");
+     httpHeaders.setBasicAuth("api",api_key);
      RestTemplate template = new RestTemplate();
      HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(multiValueMap,httpHeaders);
      ResponseEntity<JsonNode> responseEntity = template.exchange(url, HttpMethod.POST,requestEntity,JsonNode.class);
